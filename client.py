0d450b83ec824aac55b2dfd9f8cee025d0ce477c
Brandon Tram <trambrandon@gmail.com>
2014-05-31 22:02:51 -0400

    hashing on client, checking hmac on server

--- client.py
+++ client.py
@@ -1,26 +1,28 @@
 import socket
 import sys
 import hmac
+import hashlib
 
 HOST, PORT = "localhost", 9999
 data = " ".join(sys.argv[1:])
+secretKey = 'super-secret-key'
 
-digest_maker = hmac.new('super-secret-key')
-digest_maker.update(data)
+def make_digest(message):
+    return hmac.new(secretKey, message, hashlib.sha1).hexdigest()
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
+    message_and_hmac = data + '&' + make_digest(data) +"\n"
     # Connect to server and send data
     sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
+    sock.sendall(message_and_hmac)
 
     # Receive data from the server and shut down
     received = sock.recv(1024)
 finally:
     sock.close()
 
-print "Digested: {}".format(digest_maker.hexdigest())
-print "Sent:     {}".format(data)
+print "Sent:     {}".format(message_and_hmac)
 print "Received: {}".format(received)
