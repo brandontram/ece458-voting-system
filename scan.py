55cd66a894f0aba4684b5febcb7b13b3791b08fb
Justin Williams <justin.graham49@gmail.com>
2014-07-25 17:22:51 -0400

    Scan now gets mac address

--- scan.py
+++ scan.py
@@ -1,16 +1,37 @@
-import nmap, socket, fcntl, struct
 
-def get_netmask(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x891b, struct.pack('256s',ifname))[20:24])
+import sys
+import nmap, netifaces, socket
 
-iface = "en1"
-address =  socket.gethostbyname(socket.gethostname())
-print address
-print get_netmask("en1")
 
-nm = nmap.PortScanner()
-nm.scan(hosts=address, arguments='-n -sP')
-hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-for host, status in hosts_list:
-    print(host)
+# def procc():
+# 	iface = "en0"
+# 	address =  socket.gethostbyname(socket.gethostname())
+
+
+# 	nm = nmap.PortScanner()
+# 	nm.scan(hosts=address, arguments='-n -sO')
+# 	hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+# 	for host, status in hosts_list:
+	    # print(host)
+
+
+def main():
+	
+	if (len(sys.argv) == 2):
+		iface = sys.argv[2]
+
+	gateway = netifaces.gateways()
+	gateway_ip  = gateway['default'][netifaces.AF_INET][0]
+
+	print("Gateway IP:", gateway_ip)
+	
+	nm = nmap.PortScanner()
+
+	scan_result = nm.scan(hosts=gateway_ip, arguments='-sO -n')
+	mac_address = scan_result['scan'][gateway_ip]['addresses']['mac']
+
+	print('mac address', mac_address)
+
+
+if __name__ == '__main__':
+	main()
